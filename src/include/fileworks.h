5960790dd94640cef2f685627d87aa6fd72c7df1
Maria Orlova <mueshen.sh@gmail.com>
2025-12-30 19:43:18 +0300

    finished for today, obj import init, not done

--- src/include/fileworks.h
+++ src/include/fileworks.h
@@ -2,34 +2,27 @@
 #include <stdio.h> 
 #include <stdlib.h>
 #include <string.h>
-#include <extratypes.h>
-//returns first character pointer, allocated in memory for its lenght
+
 void *FWload(char path[]){
 		FILE *fptr=fopen(path, "rb");
 		if(!fptr) {
 				printf("error loading file at %s\n",path);
 				return NULL;
 		}
-		
-		//this here gets position of EOF, for size reference for reading further
 		fseek(fptr, 0L, SEEK_END);
 		size_t size=ftell(fptr);
 		rewind(fptr);
-
-
-		if(size<1)  return NULL; //1 is safe size for too little of file to be valid shader, and yet not 0 in case of stray whitespaces etc
-								  //
-		char *stringStorage=	malloc(	(size+1)	*	sizeof(char)	); //allocate size+1 for nullterm
-		size_t bytesRead = 		fread(	stringStorage, 1, size, fptr	); //read full file and store size for check, close file right after;
+		if(size<1)  return NULL; 
+								 
+		char *stringStorage	=malloc((size+1)*sizeof(char));
+		size_t bytesRead 	=fread(stringStorage,1,size,fptr);
 		fclose(fptr);
-		if(bytesRead !=size){
+		if(	bytesRead!=size	){
 				printf("failure to read entire file\n");
 				free(stringStorage);
 				return NULL;
-		} //sanity check
-
-		stringStorage[size]= '\0'; //adding null termination 
-					
+		}
+		stringStorage[size]= '\0';
 		return stringStorage;
 }
 
--- src/include/rendering.h
+++ src/include/rendering.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include "cglm/cglm.h"
 
 int renderInit(GLFWwindow* window){
 		
