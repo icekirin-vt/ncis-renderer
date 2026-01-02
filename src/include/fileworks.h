10d32215850b83578198dd3bc583814ff450a2f6
Maria Orlova <mueshen.sh@gmail.com>
2026-01-02 22:46:30 +0300

    obj importer in process

--- src/include/fileworks.h
+++ src/include/fileworks.h
@@ -1,7 +1,38 @@
 #pragma once
 #include <stdio.h> 
+#include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
+#include "cglm/vec3.h"
+#include "cglm/vec2.h"
+
+// Source - https://stackoverflow.com/a
+// Posted by chqrlie, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-01-02, License - CC BY-SA 4.0
+char *strdup(const char *s) {
+    size_t size = strlen(s) + 1;
+    char *p = malloc(size);
+    if (p != NULL) {
+        memcpy(p, s, size);
+    }
+    return p;
+}
+
+char *strndup(const char *s, size_t n) {
+    char *p;
+    size_t n1;
+
+    for (n1 = 0; n1 < n && s[n1] != '\0'; n1++)
+        continue;
+    p = malloc(n + 1);
+    if (p != NULL) {
+        memcpy(p, s, n1);
+        p[n1] = '\0';
+    }
+    return p;
+}
+// end of stackoverflow segment
+
 
 void *FWload(char path[]){
 		FILE *fptr=fopen(path, "rb");
@@ -74,12 +105,64 @@ GLuint FWlinkShaderProgram(GLuint shaderProgram){
 		
 }
 
-void FWloadOBJ(char path[]){
-		char *objSource=FWload(path);
-			
+size_t FWgetLineCount(char string[]){
+		char* result=string;
+		size_t count=0;
+		while((result=strchr(result,'\n'))!=NULL){
+				++count;
+				++result;
+		}
+		return count;
+}
 
+
+
+
+int FWloadOBJ(char path[]){
+		char *objSource=FWload(path);
+		if(objSource==NULL){
+				printf("INVALID OBJ FILE, FAILURE TO READ\n");
+				return -1;
+		}
+	
+		printf("load start\n\n");
+		char* lineStart=objSource;
+		char* lineEnd=strchr(objSource, '\n'); //gets pointer to EXACTLY newLine break
+		size_t lineLenght;
+		
+		size_t lines=FWgetLineCount(objSource);
+		printf("Total lines :%d\n",lines);
 		
 
+		while(true){
+				lineLenght=lineEnd-lineStart;
+				char* currentLine=strndup(lineStart,lineLenght);
+				//printf("current line: |%s|\n",currentLine);
+
+				char* tokenized=strtok(currentLine, " ");
+				size_t counter=0;
+				int isVector=0;
+				while(tokenized){
+						puts(&tokenized[0]);
+						if((tokenized[0]=='v') && (isalpha(tokenized[0]))) {
+								isVector=1;
+						}
+						if((tokenized[0]!='v') && (isalpha(tokenized[0]))) {
+								isVector=0;
+						}
+
+						
+						if(isVector==1) ++counter;
+						printf(" is Vector: %d\n",isVector);
+						tokenized=strtok(NULL," ");
+				}
+
+				lineStart=lineEnd+1;
+				lineEnd=strchr(lineEnd+1,'\n');	
+				if(lineEnd==NULL)
+						break;
+		}
 		free((void*) objSource);
+		return 0;
 
 }
--- src/main.c
+++ src/main.c
@@ -12,8 +12,7 @@ int main(){
 		if(!window) return -1;
 		
 
-		char *fragSource=FWload("shaders/basic.vert");	
-		free((void*) fragSource);
+			
 
 		renderInit(window);
 		FWloadOBJ("assets/tiltTrig.obj");
